f5f2673a980f07b57914a67521f0348710f9040a
darkunchain <luisfer74081@gmail.com>
2021-11-12 07:43:31 -0500

    ss

--- src/app/components/chart-dias-act/chart-dias-act.component.ts
+++ src/app/components/chart-dias-act/chart-dias-act.component.ts
@@ -17,6 +17,19 @@ export class ChartDiasActComponent implements OnInit {
   public ch1barChartLabels: Label[] = ['01', '02', '03', '04', '05', '06', '07',
   '08', '09', '10', '11', '12', '13', '14','15', '16', '17', '18', '19', '20', '21',
   '22', '23', '24', '25', '26', '27', '28','29', '30', '31'];
+  public ch1barChartBackground:Label[] = [
+    'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 99, 132, 0.2)'
+  ]
   public ch1barChartType: ChartType = 'bar';
   public ch1barChartLegend = true;
   public ch1barChartPlugins = [];
@@ -46,9 +59,19 @@ export class ChartDiasActComponent implements OnInit {
   ngOnInit() {
 
     this.datosGrafService.getDatos$().subscribe(datos => {
-      this.ch1barChartData = [{ data: datos.perDias, label: 'Días del mes' },]
-      this.ch2barChartData = [{ data: datos.perMes, label: 'mes' },]
-      this.ch3barChartData = [{ data: datos.perDiaSemMesAct, label: 'Día de la semana' },]
+      this.ch1barChartData = [{
+        data: datos.perDias,
+        label: 'Días del mes',
+        backgroundColor: this.ch1barChartBackground
+      }]
+      this.ch2barChartData = [{
+        data: datos.perMes,
+        label: 'mes'
+      }]
+      this.ch3barChartData = [{
+        data: datos.perDiaSemMesAct,
+        label: 'Día de la semana'
+      }]
     })
 
   }
